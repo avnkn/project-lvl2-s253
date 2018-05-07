b50091cfbda4904b1a995166a0e830befc18263c
Andrey V <vinyaykin@gmail.com>
2018-05-07 19:07:35 +0300

    0.1.0 add diff json

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ install:
 
 lint:
 	composer run-script phpcs -- --standard=PSR2 src bin
+
+test:
+	composer run-script phpunit tests
--- src/gendiff.php
+++ src/gendiff.php
@@ -1,18 +1,87 @@
 <?php
-namespace Gendiff\Src\Gendiff;
+namespace Src\Gendiff;
 
-function handler()
-{
-    $doc = <<<DOC
-Generate diff
+use function Funct\true;
 
-Usage:
-  gendiff (-h|--help)
-  gendiff [--format <fmt>] <firstFile> <secondFile>
+class genDiff {
 
-Options:
-  -h --help                     Show this screen
-  --format <fmt>                Report format [default: pretty]
+    function __construct(){
+      $doc = <<<DOC
+      Generate diff
+
+      Usage:
+      gendiff (-h|--help)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+
+      Options:
+      -h --help                     Show this screen
+      --format <fmt>                Report format [default: pretty]
 DOC;
-    $args = \Docopt::handle($doc);
+      $args = \Docopt::handle($doc);
+      $firstFileName = $args["<firstFile>"];
+      $secondFileName = $args["<secondFile>"];
+      $format = $args["--format"];
+      $this->handler($format, $firstFileName, $secondFileName);
+    }
+
+    function handler($format, $firstFileName, $secondFileName) {
+        if (file_exists($firstFileName)) {
+            $firstFileString = file_get_contents($firstFileName);
+        } else {
+          echo "Файла $firstFileName не существует" . PHP_EOL;
+          exit;
+        }
+        if (file_exists($secondFileName)) {
+            $secondFileString = file_get_contents($secondFileName);
+        } else {
+          echo "Файла $secondFileName не существует" . PHP_EOL;
+          exit;
+        }
+        if ($format == "yaml"){
+
+        } elseif ($format == "ini") {
+
+        } else {
+            $this->handlerJson($firstFileString, $secondFileString);
+        }
+    }
+
+    function transformBooleanString($arg) {
+        if (is_bool($arg)){
+            if ($arg == true) {
+                $result = 'true';
+            } else {
+                $result = 'false';
+            }
+        } else {
+          $result = $arg;
+        }
+        return $result;
+    }
+
+    function handlerJson($firstFileString, $secondFileString) {
+        try{
+            $firstFileArray = json_decode($firstFileString, true);
+            $secondFileArray = json_decode($secondFileString, true);
+
+            foreach ($firstFileArray as $key => $value) {
+                if (array_key_exists($key, $secondFileArray)) {
+                    if ($value === $secondFileArray[$key]){
+                        echo "  $key: " . $this->transformBooleanString($value) . PHP_EOL;
+                    } else {
+                        echo "- $key: " . $this->transformBooleanString($value). PHP_EOL;
+                        echo "+ $key: " . $this->transformBooleanString($secondFileArray[$key]) . PHP_EOL;
+                    }
+                } else {
+                    echo "- $key: " . $this->transformBooleanString($value) . PHP_EOL;
+                }
+            }
+            $diffArray = array_diff_key($secondFileArray, $firstFileArray);
+            foreach ($diffArray as $key => $value){
+                echo "+ $key: " . $this->transformBooleanString($value) . PHP_EOL;
+            }
+        } catch (Exception $e) {
+            echo 'Ошибка: ',  $e->getMessage(), PHP_EOL;
+        }
+    }
 }
